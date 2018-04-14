24abc67366d6a72a12fd411c8d117cecb1c43c80
lightbitbird <maxgramf3@gmail.com>
2018-04-14 17:23:56 +0900

    Add a slice def

--- src/main/scala/others/SurrogatePairs.scala
+++ src/main/scala/others/SurrogatePairs.scala
@@ -6,8 +6,18 @@ object SurrogatePairs {
     println(s"codePointCount ==> ${SurrogatePairsUtil.codePointCount("𠹭タロま")}")
     println(s"offsetByCodePoints ==> ${"𠹭タロま𤄃".offsetByCodePoints(0, 3)}")
 
+    println(s"Surrogate substrByCodePointCount(3, 6) ==> ${SurrogatePairsUtil.substrByCodePointCount("𠹭タロま𤄃a", 3, 6)}")
+    println(s"Surrogate substrByCodePointCount(0, 10) ==> ${SurrogatePairsUtil.substrByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 0, 10)}")
+    println(s"Surrogate substrByCodePointCount(2, 5) ==> ${SurrogatePairsUtil.substrByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 2, 5)}")
+
+    println(s"-------------------------------------------")
+
     println(s"Surrogate sliceByCodePointCount ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃a", 3, 6)}")
-    println(s"Surrogate sliceByCodePointCount2 ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 0, 10)}")
+    println(s"Surrogate sliceByCodePointCount(0, 10) ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 0, 10)}")
+    println(s"Surrogate sliceByCodePointCount(2, 7) ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 2, 7)}")
+    println(s"Surrogate sliceByCodePointCount(5, 11) ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 5, 11)}")
+    println(s"Surrogate sliceByCodePointCount(-5, 11) ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", -5, 11)}")
+    println(s"Surrogate sliceByCodePointCount(5, -3) ==> ${SurrogatePairsUtil.sliceByCodePointCount("𠹭タロま𤄃aXX𤄃Xbbb", 5, -3)}")
   }
 
 }
@@ -18,12 +28,17 @@ object SurrogatePairsUtil {
     string.codePointCount(0, string.length)
   }
 
-  def sliceByCodePointCount(target: String, from: Int, len: Int): String = {
+  def substrByCodePointCount(target: String, from: Int, len: Int): String = {
     val charArray = target.toCharArray
-    val start = target.offsetByCodePoints(0, from)
+    val cpCount = codePointCount(target)
+    val start = {
+      if (target.isEmpty || cpCount < from) 0
+      else target.offsetByCodePoints(0, from)
+    }
     val count = {
-      if (codePointCount(target) >= from + len) start + len
-      else codePointCount(target) - from + start
+      if (target.isEmpty || cpCount < from) 0
+      else if (cpCount >= from + len) start + len
+      else cpCount - from + start
     }
     var index = start
     (start until count).foldLeft("") { (concat, i) =>
@@ -31,5 +46,12 @@ object SurrogatePairsUtil {
       index += Character.charCount(codePoint)
       concat + String.valueOf(Character.toChars(codePoint))
     }
+  }
+
+  def sliceByCodePointCount(target: String, from: Int, until: Int): String = {
+    val len = codePointCount(target)
+    val f = target.offsetByCodePoints(0, Math.max(Math.max(0, from), 0))
+    val u = target.offsetByCodePoints(0, Math.min(Math.max(0, until), len))
+    target.slice(f, u)
   }
 }
